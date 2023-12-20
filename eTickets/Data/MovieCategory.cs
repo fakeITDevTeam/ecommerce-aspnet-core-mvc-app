8fae3faedb30e59a038006a4e18b755a432d94c2
fakeITDevTeam <dev5544455@gmail.com>
2023-12-20 15:27:26 +0700

    Add models

---
+++ eTickets/Data/MovieCategory.cs
@@ -0,0 +1,13 @@
+﻿namespace eTickets.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+
+        Comedy,
+
+        Drama,
+
+        Documentary
+    }
+}
---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using eTickets.Data;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public double Price { get; set; }
+
+        public string? ImageURL { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
